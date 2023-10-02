ed7e397fbc69e582ec57be5dffd6c131f00bf62b
ggrigorov <g.grigorov@blubito.com>
2023-10-02 22:00:08 +0300

    Initialized Backend

---
+++ backend/src/test/java/com/blubito/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blubito.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
