859a537c67bb2d5adb29a3398b71ae34e841d322
LenkoIvanov17 <l.ivanov@blubito.com>
2023-10-03 09:57:29 +0300

    feeat: add authentication - no button styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+// import "primeicons/primeicons.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={`${import.meta.env.VITE_DOMAIN}`}
+      clientId={`${import.meta.env.VITE_CLIENTID}`}
+      authorizationParams={{
+        audience: import.meta.env.VITE_AUDIENCE,
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
